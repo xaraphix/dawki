4a6fcbfaac6649f4b1c31e9f3cd4d58ba15b0f1a
Suyash Singh <suyash.x@gmail.com>
2021-09-17 17:38:17 +0530

    updates gitignore

--- src/logging/logging.cc
+++ src/logging/logging.cc
@@ -3,7 +3,7 @@
 void Dwki::init_logging()
 {
   namespace logging = boost::log;
-  logging::add_file_log("sample.log");
+  logging::add_file_log("logs/dawki.log");
   logging::core::get()->set_filter(logging::trivial::severity >=
                                    logging::trivial::info);
 }
